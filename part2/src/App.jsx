9c15a247612fd42febc47fa7faf2f336495579f7
karthikprabhu04 <kpp32@cam.ac.uk>
2026-02-14 19:58:03 +0000

    Complete exercises 2.6 - 2.10

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,49 +1,101 @@
 import { useState } from "react";
-import Note from "./components/Note";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
-  const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true);
+const Filter = ({ newSearch, handleSearchChange }) => {
+  return (
+    <p>
+      filter shown with{" "}
+      <input value={newSearch} onChange={handleSearchChange} />
+    </p>
+  );
+};
 
-  const notesToShow = showAll
-    ? notes
-    : notes.filter((note) => note.important === true);
+const PersonForm = ({addName, newName, handleNameChange, newNumber, handleNumberChange}) => {
+  return (
+  <div>
+    <form onSubmit={addName}>
+      <div>
+        name: <input value={newName} onChange={handleNameChange} />
+      </div>
+      <div>
+        number: <input value={newNumber} onChange={handleNumberChange} />
+      </div>
+      <div>
+        <button type="submit">add</button>
+      </div>
+    </form>
+  </div>
+  )
+}
 
-  const handleNoteChange = (event) => {
-    console.log(event.target.value);
-    setNewNote(event.target.value);
+const Persons = ({persons, newSearch}) => {
+  return (
+  <div>
+    {persons
+    .filter((person) =>
+      person.name.toLowerCase().includes(newSearch.toLowerCase()),
+    )
+    .map((person) => (
+      <p key={person.name}>
+        {person.name} {person.number}
+      </p>
+    ))}
+  </div>
+  )
+}
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
+  ]);
+  const [newName, setNewName] = useState("");
+  const [newNumber, setNewNumber] = useState("");
+  const [newSearch, setNewSearch] = useState("");
+
+  const addName = (e) => {
+    e.preventDefault();
+
+    const personObject = {
+      name: newName,
+      number: newNumber,
+    };
+    const exists = persons.some((person) => person.name === newName);
+    if (exists) {
+      alert(`${newName} already exists`);
+    } else {
+      setPersons(persons.concat(personObject));
+      setNewName("");
+      setNewNumber("");
+    }
   };
 
-  const addNote = (event) => {
-    event.preventDefault();
-    const noteObject = {
-      content: newNote,
-      important: Math.random() < 0.5,
-      id: String(notes.length + 1),
-    };
+  const handleNameChange = (e) => {
+    setNewName(e.target.value);
+  };
 
-    setNotes(notes.concat(noteObject));
-    setNewNote("");
+  const handleNumberChange = (e) => {
+    setNewNumber(e.target.value);
+  };
+
+  const handleSearchChange = (e) => {
+    setNewSearch(e.target.value);
   };
 
   return (
     <div>
-      <h1>Notes</h1>
-      <div>
-        <button onClick={() => setShowAll(!showAll)}>
-          show {showAll ? 'important' : 'all'}
-        </button>
-      </div>
-      <ul>
-        {notesToShow.map((note) => (
-          <Note key={note.id} note={note} />
-        ))}
-      </ul>
-      <form onSubmit={addNote}>
-        <input value={newNote} onChange={handleNoteChange} />
-        <button type="submit">save</button>
-      </form>
+      <h2>Phonebook</h2>
+
+      <Filter newSearch={newSearch} handleSearchChange={handleSearchChange}/>
+
+      <h2>add a new</h2>
+
+      <PersonForm addName={addName} newName={newName} handleNameChange={handleNameChange} newNumber={newNumber} handleNumberChange={handleNumberChange}/>
+
+      <h2>Numbers</h2>
+
+      <Persons persons={persons} newSearch={newSearch}/>
     </div>
   );
 };
