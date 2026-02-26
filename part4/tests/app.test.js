c94456a9fc00ad12611b2584114bf05ee64f22dd
karthikprabhu04 <kpp32@cam.ac.uk>
2026-02-26 17:03:37 +0000

    Refactored test code with describe

--- part4/tests/app.test.js
+++ part4/tests/app.test.js
@@ -1,4 +1,4 @@
-const { test, after, beforeEach } = require('node:test')
+const { describe, test, after, beforeEach } = require('node:test')
 const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
@@ -7,6 +7,7 @@ const app = require('../app')
 
 const api = supertest(app)
 
+// Create artifical database documents
 const initalBlogs = [
   {
     title: "test title 1",
@@ -20,6 +21,7 @@ const initalBlogs = [
   }
 ]
 
+// Setup before each test
 beforeEach(async () => {
   await Blog.deleteMany({})
   let blogObject = new Blog(initalBlogs[0])
@@ -28,138 +30,143 @@ beforeEach(async () => {
   await blogObject.save()
 })
 
-
-test('notes are returned as json', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  console.log("Number of posts:", response.body.length)
-})
-
-test('verify that unique identifier is id and not _id', async () => {
-  const response = await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  const blog = response.body[0]
-
-  assert.ok(blog.id)
-  assert.strictEqual(blog._id, undefined)
-})
-
-test('post request saved successfully', async () => {
-  const newBlog = {
-    title: "new title post for test",
-    author: "John Smith",
-    url: "testurl",
-  }
+describe('Basic tests for app', () => {
+  test('notes are returned as json', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    console.log("Number of posts:", response.body.length)
+  })
 
-  const response = await api.get('/api/blogs')
-  const contents = response.body.map(r => r.title)
-
-  assert.strictEqual(response.body.length, initalBlogs.length + 1)
-  assert(contents.includes('new title post for test'))
-})
-
-// Test likes property
-test('Likes should default to 0 if missing', async () => {
-  const newBlog = {
-    title: "testing likes property",
-    author: "Jack Smith",
-    url: "testurl",
-  }
+  test('verify that unique identifier is id and not _id', async () => {
+    const response = await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
   
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blog = response.body[0]
+  
+    assert.ok(blog.id)
+    assert.strictEqual(blog._id, undefined)
+  })
 
-  const response = await api.get('/api/blogs')
-  const latestBlog = response.body[response.body.length - 1]
-
-  assert.strictEqual(latestBlog.likes, 0)
+  test('post request saved successfully', async () => {
+    const newBlog = {
+      title: "new title post for test",
+      author: "John Smith",
+      url: "testurl",
+    }
+    
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r => r.title)
+  
+    assert.strictEqual(response.body.length, initalBlogs.length + 1)
+    assert(contents.includes('new title post for test'))
+  })
 })
 
-// Test for when title and url properties missing
-test('returns 400 if title is missing', async () => {
-  const newBlog = {
-    url: 'example.com',
-    author: 'Jack Smith'
-  }
+describe('Enforce default properties', () => {
+  // Test likes property
+  test('Likes should default to 0 if missing', async () => {
+    const newBlog = {
+      title: "testing likes property",
+      author: "Jack Smith",
+      url: "testurl",
+    }
+    
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const response = await api.get('/api/blogs')
+    const latestBlog = response.body[response.body.length - 1]
+  
+    assert.strictEqual(latestBlog.likes, 0)
+  })
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+  // Test for when title and url properties missing
+  test('returns 400 if title is missing', async () => {
+    const newBlog = {
+      url: 'example.com',
+      author: 'Jack Smith'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
+  
+  test('returns 400 if url is missing', async () => {
+    const newBlog = {
+      title: 'Testing validation',
+      author: 'Jack Smith'
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(400)
+  })
 })
 
-test('returns 400 if url is missing', async () => {
-  const newBlog = {
-    title: 'Testing validation',
-    author: 'Jack Smith'
-  }
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(400)
+describe('Updating and deleting posts', () => {
+  // Test for deleting post
+  test('Deletion of one post successfully', async () => {
+    // Get id of a post
+    const response = await api.get('/api/blogs')
+    const latestBlog = response.body[response.body.length - 1]
+    const latestBlogID = latestBlog.id
+    // Remove id of a post
+    await api
+      .delete(`/api/blogs/${latestBlogID}`)
+      .expect(204)
+    // Check length of posts is one less
+    const final = await api.get('/api/blogs')
+    assert.strictEqual(final.body.length, initalBlogs.length - 1)
+  })
+  
+  test('Update of one post', async () => {
+    // Get existing blogs
+    const blogsAtStart = await api.get('/api/blogs')
+    const blogToUpdate = blogsAtStart.body[0]
+  
+    // Update data
+    const updatedData = {
+      ...blogToUpdate,
+      likes: blogToUpdate.likes + 1
+    }
+  
+    // Send PUT request
+    const response = await api
+      .put(`/api/blogs/${blogToUpdate.id}`)
+      .send(updatedData)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  
+    // Verify likes updated
+    assert.strictEqual(response.body.likes, updatedData.likes)
+  
+    // Verify DB actually updated
+    const blogsAtEnd = await api.get('/api/blogs')
+    const updatedBlog = blogsAtEnd.body.find(
+      b => b.id === blogToUpdate.id
+    )
+  
+    assert.strictEqual(updatedBlog.likes, updatedData.likes)
+  })
 })
-
-// Test for deleting post
-test('Deletion of one post successfully', async () => {
-  // Get id of a post
-  const response = await api.get('/api/blogs')
-  const latestBlog = response.body[response.body.length - 1]
-  const latestBlogID = latestBlog.id
-  // Remove id of a post
-  await api
-    .delete(`/api/blogs/${latestBlogID}`)
-    .expect(204)
-  // Check length of posts is one less
-  const final = await api.get('/api/blogs')
-  assert.strictEqual(final.body.length, initalBlogs.length - 1)
-})
-
-test.only('Update of one post', async () => {
-  // Get existing blogs
-  const blogsAtStart = await api.get('/api/blogs')
-  const blogToUpdate = blogsAtStart.body[0]
-
-  // Update data
-  const updatedData = {
-    ...blogToUpdate,
-    likes: blogToUpdate.likes + 1
-  }
-
-  // Send PUT request
-  const response = await api
-    .put(`/api/blogs/${blogToUpdate.id}`)
-    .send(updatedData)
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
-
-  // Verify likes updated
-  assert.strictEqual(response.body.likes, updatedData.likes)
-
-  // Verify DB actually updated
-  const blogsAtEnd = await api.get('/api/blogs')
-  const updatedBlog = blogsAtEnd.body.find(
-    b => b.id === blogToUpdate.id
-  )
-
-  assert.strictEqual(updatedBlog.likes, updatedData.likes)
-})
-
 
 // Cleanup after the tests to cut off database connection
 after(async () => {
